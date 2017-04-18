9694dfccc9d204fb8298baa656e7d99c4d4eaed4
Christopher N. Hesse <raymanfx@gmail.com>
2017-04-18 09:22:49 +0200

    raphael: Enforce vendor tree presence
    A compiled build has no chance of booting without the prebuilt
    binaries, so fail the build if those are not present.
    
    Change-Id: Iadd43e887d426732e549969ab8deb14194ca818d
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -158,4 +158,4 @@ BOARD_AVB_RECOVERY_ROLLBACK_INDEX := 1
 BOARD_AVB_RECOVERY_ROLLBACK_INDEX_LOCATION := 1
 
 # Inherit from the proprietary version
--include vendor/xiaomi/raphael/BoardConfigVendor.mk
+include vendor/xiaomi/raphael/BoardConfigVendor.mk
