3f6a37c36d660a289621afedd4c9bf9ce4d9d7aa
ralf <ralf979@gmail.com>
2021-02-20 13:15:11 +0000

    nitrogen bringup

--- nitrogen_raphael.mk
+++ nitrogen_raphael.mk
@@ -6,14 +6,16 @@
 
 $(call inherit-product, device/xiaomi/raphael/device.mk)
 
+TARGET_BOOT_ANIMATION_RES := 1080
+
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/nitrogen/products/common.mk)
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := raphael
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_NAME := lineage_raphael
+PRODUCT_NAME := nitrogen_raphael
 
 BUILD_FINGERPRINT := "Xiaomi/raphaelin/raphaelin:9/PKQ1.181121.001/V10.3.3.0.PFKINXM:user/release-keys"
 
